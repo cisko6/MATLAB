272e1d7f5f0ad109d30e90252b0c62268acc330e
superc1so <patrikcisar1999@gmail.com>
2023-03-16 12:50:27 +0100

    x

--- lindleyQAC.m
+++ lindleyQAC.m
@@ -1,18 +1,17 @@
 % Parameters
-c = 1; % capacity
-a = [2 5 1 3 4]; % incoming packets at each time step
-T = length(a); % number of time steps
+c = 1; % kapacita
+a = [2 5 1 3 4]; % prichadzajuce pakety
+T = length(a); % čas
 
 % Initialize the buffer
-q = zeros(1, T+1); % buffer state at each time step
-q(1) = 0; % initial buffer state
+q = zeros(1, T+1); % vynulovanie
+q(1) = 0; % inicializacia
 
-% Simulate the buffer
+% Simulacia buffra
 for t = 1:T
     q(t+1) = max(q(t) + a(t) - c, 0);
 end
 
-% Plot the buffer state
 figure;
 stem(0:T, q);
 xlabel('Time step');
