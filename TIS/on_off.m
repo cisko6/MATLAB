87f178489a464befd2e799c599ee79b902826856
superc1so <patrikcisar1999@gmail.com>
2023-03-17 08:12:21 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,16 +1,16 @@
 
-alfa = 0.5;
-beta = 0.5;
+alfa = 0.95;
+beta = 0.95;
 stav = 1;
 sample_size = 5;
-pocet_generovanych = 3000;
+pocet_generovanych = 2000;
 
 one_counter = 0;
 zero_counter = 0;
 
 b = 1;
 
-for i=1:pocet_generovanych
+for i=1:pocet_generovanych%%%%%%%ZLE GENERUJE, PRI 2K generuje 3k cisel
     if stav == 1
         data(i) = 1;
         one_counter = one_counter + 1;
@@ -30,8 +30,12 @@ for i=1:pocet_generovanych
     end
 end
 
-for i=1:pocet_generovanych
-    pom_sum = sum(data(i*sample_size:(i*sample_size)+sample_size-1));
+%samplovanie dat
+pom_sum = sum(data(1:sample_size));
+sampled_data(1) = pom_sum;
+
+for i=1:(pocet_generovanych/sample_size)-1
+    pom_sum = sum(data((i*sample_size)+1:(i*sample_size)+sample_size));
     sampled_data(i) = pom_sum;
 end
 
