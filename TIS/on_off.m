629bbc5671bd40018bf697c66635f1702144e6c3
superc1so <patrikcisar1999@gmail.com>
2023-03-17 11:01:47 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,9 +1,9 @@
 
-alfa = 0.05;
-beta = 0.05;
+alfa = 0.95;
+beta = 0.95;
 stav = 1;
-sample_size = 4;
-pocet_generovanych = 200;
+sample_size = 5;
+pocet_generovanych = 100;
 
 one_counter = 0;
 zero_counter = 0;
@@ -31,6 +31,7 @@ for i=1:pocet_generovanych
 end
 
 %samplovanie dat
+clear('sampled_data');
 pom_sum = sum(data(1:sample_size));
 sampled_data(1) = pom_sum;
 
