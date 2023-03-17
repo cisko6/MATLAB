7d742fd598f820a04bd9ee5551b21fb401d48a57
superc1so <patrikcisar1999@gmail.com>
2023-03-17 10:44:45 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,16 +1,16 @@
 
-alfa = 0.95;
-beta = 0.95;
+alfa = 0.05;
+beta = 0.05;
 stav = 1;
-sample_size = 5;
-pocet_generovanych = 2000;
+sample_size = 4;
+pocet_generovanych = 200;
 
 one_counter = 0;
 zero_counter = 0;
 
 b = 1;
 
-for i=1:pocet_generovanych%%%%%%%ZLE GENERUJE, PRI 2K generuje 3k cisel
+for i=1:pocet_generovanych
     if stav == 1
         data(i) = 1;
         one_counter = one_counter + 1;
