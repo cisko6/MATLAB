f7f83c3e99193e4f525c2b1a33a07b98883c5378
superc1so <patrikcisar1999@gmail.com>
2023-03-17 11:38:14 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,17 +1,19 @@
+clear('data');
+clear('sampled_data');
 
 alfa = 0.95;
 beta = 0.95;
 stav = 1;
-sample_size = 5;
-pocet_generovanych = 100;
+sample_size = 4;
+pocet_generovanych = 5000;
 
 one_counter = 0;
 zero_counter = 0;
 
 b = 1;
 
-%generovanie sekvencie bitov
-for i=1:pocet_generovanych
+% generovanie sekvencie bitov
+for i=1:pocet_generovanych*sample_size
     if stav == 1
         data(i) = 1;
         one_counter = one_counter + 1;
@@ -31,14 +33,39 @@ for i=1:pocet_generovanych
     end
 end
 
-%samplovanie dat
-clear('sampled_data');
+% samplovanie dat;
+sampled_data = zeros(1,pocet_generovanych/sample_size);
+
 pom_sum = sum(data(1:sample_size));
 sampled_data(1) = pom_sum;
 
-for i=1:(pocet_generovanych/sample_size)-1 % -1 lebo prvykrat mam vysie
+for i=1:pocet_generovanych-1 % -1 lebo prvykrat mam vysie
     pom_sum = sum(data((i*sample_size)+1:(i*sample_size)+sample_size));
     sampled_data(i+1) = pom_sum;
 end
+
+
+
+
+
+
+
+% simulacia lidleyho buffru
+c = 2; % kapacita
+T = pocet_generovanych; % čas
+priemerPrichodov = 1;
+
+%a = exprnd(priemerPrichodov, [1, T]); % prichadzajuce pakety
+
+% Inicializacia buffra
+q = zeros(1, T+1);
+q(1) = 0;
+
+% Simulacia buffra
+for t = 1:T
+    %q(t+1) = max(q(t) + a(t) - c, 0);
+end
+
+
 
 plot(sampled_data)
