612b97c971d0825109aed77909e11279bf54c0e2
superc1so <patrikcisar1999@gmail.com>
2023-03-17 14:36:13 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,32 +1,31 @@
 clear('data');
 clear('sampled_data');
 
-alfa = 0.95;
-beta = 0.95;
+alfa = 0.9;
+beta = 0.9;
 stav = 1;
-sample_size = 4;
+sample_size = 5;
 pocet_generovanych = 5000;
+avg_lambda = 1;
 
-one_counter = 0;
-zero_counter = 0;
-
-b = 1;
+counter_one = 0;
+counter_zero = 0;
 
 % generovanie sekvencie bitov
-for i=1:pocet_generovanych*sample_size
+for i=1:pocet_generovanych%pocet_generovanych*sample_size
     if stav == 1
         data(i) = 1;
-        one_counter = one_counter + 1;
+        counter_one = counter_one + 1;
 
-        pravd = b.*rand();
+        pravd = 1.*rand();
         if pravd >= (1 - alfa)
             stav = 0;
         end
     else
         data(i) = 0;
-        zero_counter = zero_counter + 1;
+        counter_zero = counter_zero + 1;
 
-        pravd = b.*rand();
+        pravd = 1.*rand();
         if pravd >= (1-beta)
             stav = 1;
         end
@@ -39,33 +38,50 @@ sampled_data = zeros(1,pocet_generovanych/sample_size);
 pom_sum = sum(data(1:sample_size));
 sampled_data(1) = pom_sum;
 
-for i=1:pocet_generovanych-1 % -1 lebo prvykrat mam vysie
+for i=1:(pocet_generovanych/sample_size)-1 % -1 lebo prvykrat mam vysie
     pom_sum = sum(data((i*sample_size)+1:(i*sample_size)+sample_size));
     sampled_data(i+1) = pom_sum;
 end
 
 
-
-
-
-
-
 % simulacia lidleyho buffru
-c = 2; % kapacita
-T = pocet_generovanych; % čas
-priemerPrichodov = 1;
+pi1 = beta/(alfa + beta);
+avg_intenzita = pi1 * sample_size;
 
-%a = exprnd(priemerPrichodov, [1, T]); % prichadzajuce pakety
+c = avg_lambda * avg_intenzita; % kapacita
+T = pocet_generovanych/sample_size; % čas
+
 
 % Inicializacia buffra
 q = zeros(1, T+1);
 q(1) = 0;
 
 % Simulacia buffra
-for t = 1:T
-    %q(t+1) = max(q(t) + a(t) - c, 0);
+for t=1:T
+    q(t+1) = max(q(t) + sampled_data(t) - c, 0);
 end
 
+% Overenie ON/OFF generátora
+xd = counter_one/pocet_generovanych;
+fprintf("%d = %d\n",xd,pi1);
 
-
+% Výstupy
+subplot(3,1,1)
 plot(sampled_data)
+title("ON/OFF Generátor pre " + pocet_generovanych + " bitov");
+xlabel('Počet navzorkovaných dát');
+ylabel('Počet jednotiek vo vzorkách');
+
+
+subplot(3,1,2)
+plot(q);
+title('Lindley buffer simulácia');
+xlabel('Čas');
+ylabel('Počet paketov v queue');
+
+
+subplot(3,1,3)
+histogram(q, 'Normalization', 'probability');
+title('Histogram Lindleyho buffra');
+xlabel('Počet paketov v queue');
+ylabel('Pravdepodobnosti');
