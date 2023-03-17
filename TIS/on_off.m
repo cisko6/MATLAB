2d5e0a68de42093845613eed6d2b5f29a24c1210
superc1so <patrikcisar1999@gmail.com>
2023-03-17 15:50:32 +0100

    Update on_off.m

--- TIS/on_off.m
+++ TIS/on_off.m
@@ -1,11 +1,13 @@
 clear('data');
 clear('sampled_data');
+clear('q');
 
-alfa = 0.9;
-beta = 0.9;
+% Parametre
+alfa = 0.95;
+beta = 0.95;
+sample_size = 6;
+pocet_generovanych = 6000;
 stav = 1;
-sample_size = 5;
-pocet_generovanych = 5000;
 avg_lambda = 1;
 
 counter_one = 0;
@@ -32,17 +34,16 @@ for i=1:pocet_generovanych%pocet_generovanych*sample_size
     end
 end
 
-% samplovanie dat;
+% samplovanie dat
 sampled_data = zeros(1,pocet_generovanych/sample_size);
 
 pom_sum = sum(data(1:sample_size));
 sampled_data(1) = pom_sum;
 
-for i=1:(pocet_generovanych/sample_size)-1 % -1 lebo prvykrat mam vysie
+for i=1:(pocet_generovanych/sample_size)-1
     pom_sum = sum(data((i*sample_size)+1:(i*sample_size)+sample_size));
     sampled_data(i+1) = pom_sum;
 end
-
 
 % simulacia lidleyho buffru
 pi1 = beta/(alfa + beta);
@@ -50,7 +51,6 @@ avg_intenzita = pi1 * sample_size;
 
 c = avg_lambda * avg_intenzita; % kapacita
 T = pocet_generovanych/sample_size; % čas
-
 
 % Inicializacia buffra
 q = zeros(1, T+1);
@@ -63,24 +63,31 @@ end
 
 % Overenie ON/OFF generátora
 xd = counter_one/pocet_generovanych;
-fprintf("%d = %d\n",xd,pi1);
+fprintf("KONTROLA GENERATORA\n");
+fprintf("N1/pocet generovanych = π1\n");
+fprintf("         %d = %d\n",xd,pi1);
 
 % Výstupy
-subplot(3,1,1)
+subplot(4,1,1)
+plot(data);
+title("ON/OFF Generátor pre " + pocet_generovanych + " bitov, kde sample size = "+sample_size+", α = "+alfa+" a β = "+beta);
+xlabel('Počet bitov');
+ylabel('1/0');
+
+subplot(4,1,2)
 plot(sampled_data)
-title("ON/OFF Generátor pre " + pocet_generovanych + " bitov");
+title("Navzorkované dáta");
 xlabel('Počet navzorkovaných dát');
 ylabel('Počet jednotiek vo vzorkách');
 
-
-subplot(3,1,2)
+subplot(4,1,3)
 plot(q);
-title('Lindley buffer simulácia');
+xlim([0 pocet_generovanych/sample_size]);
+title("Lindley buffer simulácia, kde c = "+c);
 xlabel('Čas');
 ylabel('Počet paketov v queue');
 
-
-subplot(3,1,3)
+subplot(4,1,4)
 histogram(q, 'Normalization', 'probability');
 title('Histogram Lindleyho buffra');
 xlabel('Počet paketov v queue');
