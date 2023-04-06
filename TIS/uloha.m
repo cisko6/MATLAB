d4b02313e45718a2ad6c7eee44e95fe05ab619ea
superc1so <patrikcisar1999@gmail.com>
2023-04-06 23:04:09 +0200

    Update uloha.m

--- TIS/uloha.m
+++ TIS/uloha.m
@@ -2,8 +2,8 @@
 
 
 %M = readtable("C:\Users\patri\Downloads\miniShark\01 tsharkPONDELOK4_0_0.csv");ntbk
-%M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4.csv");
-M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4_0.csv");
+M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4.csv");
+%M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4_0.csv");
 dlzka_csv = height(M)-1;
 
 data_casy = M.Var6;
