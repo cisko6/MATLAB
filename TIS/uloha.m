0bb2365abe5dbe8808bb2d308d0db3bc726f6b72
superc1so <patrikcisar1999@gmail.com>
2023-04-26 23:51:34 +0200

    Update uloha.m

--- TIS/uloha.m
+++ TIS/uloha.m
@@ -7,27 +7,33 @@ nasobok_spic = 5;
 
 %M = readtable("C:\Users\patri\Downloads\miniShark\01 tsharkPONDELOK4_0_0.csv");ntbk
 %M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4.csv");%full csv
-M = readtable("C:\Users\patri\Downloads\miniTok\01 tsharkPONDELOK4_0.csv");
+M = readtable("C:\Users\patri\Downloads\miniTok\01 tsharkPONDELOK4_0.csv");%minitok
+%X = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4_0.csv"); M = X(2500000:5000001,1:14); % prvy kopec
+
 dlzka_csv = height(M)-1;
 
 data_casy = M.Var6;
 hodiny = data_casy.Hour;
 minuty = data_casy.Minute;
-sekundy = floor(data_casy.Second);
+sekundy = data_casy.Second;
+sekundy_floored = floor(data_casy.Second);
 
 %inicializacia cyklu slotovania
 pom_h = hodiny(1);
 pom_m = minuty(1);
-pom_s = sekundy(1);
+pom_s = sekundy_floored(1);
 index = 1;
 data = zeros(1,dlzka_csv);
+data_kB = zeros(1,dlzka_csv);
 
 %slotovanie
+data_informacie = M.Var8;
 for i=1:dlzka_csv
     if pom_h == hodiny(i)
         if pom_m == minuty(i)
-            if pom_s == sekundy(i)
+            if pom_s == sekundy_floored(i)
                 data(index) = data(index) + 1;
+                data_kB(index) = data_kB(index) + data_informacie(i); 
                 continue
             end
         end
@@ -35,21 +41,41 @@ for i=1:dlzka_csv
     
     pom_h = hodiny(i);
     pom_m = minuty(i);
-    pom_s = sekundy(i);
+    pom_s = sekundy_floored(i);
 
     index = index + 1;
     data(index) = data(index) + 1;
+    data_kB(index) = data_kB(index) + data_informacie(i);
 end
 
-%
+% medzery
+medzery = zeros(1,dlzka_csv);
+for i=1:dlzka_csv
+    if sekundy(i) > 1 && sekundy(i+1) < 1
+        medzery(i) = ( 60 - sekundy(i) ) + sekundy(i+1);
+        continue
+    end
+    medzery(i) = sekundy(i+1) - sekundy(i);
+end
+
+% vypocty
 for i=1:index-compute_window+1
     data_pom = data(i:compute_window+i-1);
+    data_pom2 = data_kB(i:compute_window+i-1);
 
+    %data_pocty
     m(i+compute_window-1) = mean(data_pom); % klzavy priemer
     s(i+compute_window-1)  = sqrt(cov(data_pom)); % smerodajna odchylka
     V(i+compute_window-1) = nasobok_koef*sqrt(cov(data_pom))/mean(data_pom); % koeficient variabilnosti
     K(i+compute_window-1)  = kurtosis(data_pom); % sikmost
     Skw(i+compute_window-1) = nasobok_spic*max(skewness(data_pom),0); % spicatost
+
+    %data_kB
+    m_kB(i+compute_window-1) = mean(data_pom2); % klzavy priemer
+    s_kB(i+compute_window-1)  = sqrt(cov(data_pom2)); % smerodajna odchylka
+    V_kB(i+compute_window-1) = nasobok_koef*sqrt(cov(data_pom2))/mean(data_pom2); % koeficient variabilnosti
+    K_kB(i+compute_window-1)  = kurtosis(data_pom2); % sikmost
+    Skw_kB(i+compute_window-1) = nasobok_spic*max(skewness(data_pom2),0); % spicatost
 
     %d0(:) = 0;
     %d0 = data_pom - m; % centrovane data
@@ -58,9 +84,10 @@ end
 
 %VYPISY
 data_plot = data(1:index);
+data_plot_kB = data_kB(1:index);
+subcislo = 5;
 
-
-subplot(2,1,1);
+subplot(subcislo,1,1);
 plot(data_plot,'blue');
 hold on
 plot(m,'red');
@@ -70,7 +97,7 @@ hold off
 xlim([0 index]);
 title("Compute window = "+compute_window+", \color{blue}tok ,\color{red}priemer, \color{green}smer.odchylka");
 
-subplot(2,1,2);
+subplot(subcislo,1,2);
 plot(V,'cyan');
 hold on
 plot(K,'black');
@@ -78,11 +105,49 @@ hold on
 plot(Skw,'magenta');
 hold off
 xlim([0 index]);
-title('\color{black}sikmost, \color{magenta}spicatost ,\color{cyan}koef. var.');
+title("\color{black}sikmost, \color{magenta}"+nasobok_spic+"x spicatost ,\color{cyan} "+nasobok_koef+"xkoef. var.");
+
+subplot(subcislo,1,3);
+histogram(data_plot, 'Normalization', 'probability');
+title('histogram toku');
+
+subplot(subcislo,1,4);
+plot(medzery);
+title('medzery');
+
+subplot(subcislo,1,5);
+histogram(medzery, 'Normalization', 'probability');
+title('histogram medzier');
+
+figure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%another one
+subcislo2 = 3;
+subplot(subcislo2,1,1);
+plot(data_plot_kB,'blue');
+hold on
+plot(m_kB,'red');
+hold on
+plot(s_kB,'green');
+hold off
+xlim([0 index]);
+title("kB - Compute window = "+compute_window+", \color{blue}tok ,\color{red}priemer, \color{green}smer.odchylka");
+
+subplot(subcislo2,1,2);
+plot(V_kB,'cyan');
+hold on
+plot(K_kB,'black');
+hold on
+plot(Skw_kB,'magenta');
+hold off
+xlim([0 index]);
+title("\color{black}sikmost, \color{magenta}"+nasobok_spic+"x spicatost ,\color{cyan} "+nasobok_koef+"xkoef. var.");
+
+subplot(subcislo2,1,3);
+histogram(data_plot_kB, 'Normalization', 'probability');
+title("histogram kB");
 
 
 %PRINTY
-
 m(1) = mean(data_plot); % klzavy priemer
 s(1)  = sqrt(cov(data_plot)); % smerodajna odchylka
 V(1) = sqrt(cov(data_plot))/mean(data_plot); % koeficient variabilnosti
