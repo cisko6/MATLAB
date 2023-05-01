4418f545ff135994cddd40de7e09ba406dfa24d2
superc1so <patrikcisar1999@gmail.com>
2023-05-01 17:44:31 +0200

    Update uloha.m

--- TIS/uloha.m
+++ TIS/uloha.m
@@ -1,20 +1,18 @@
 clear
 clc
 
-compute_window = 20;
+compute_window = 5;
 nasobok_koef = 20;
 nasobok_spic = 5;
 
 %M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4.csv");%full csv
-%M = readtable("C:\Users\patri\Downloads\miniTok\01 tsharkPONDELOK4_0.csv");%minitok
+M = readtable("C:\Users\patri\Downloads\miniTok\01 tsharkPONDELOK4_0.csv");%minitok
 
 %X = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4_0.csv"); M = X(2500000:5000001,1:14); % prva cast, CW = 100
 %M = readtable("C:\Users\patri\Downloads\časti_toku\druha_cast\druha_cast.csv");%druha cast CW = 20
 %M = readtable("C:\Users\patri\Downloads\časti_toku\tretia_cast\tretia_cast.csv");
 %M = readtable("C:\Users\patri\Downloads\časti_toku\stvrta_cast\01 tsharkPONDELOK4_5_1.csv");
-M = readtable("C:\Users\patri\Downloads\file.csv");
-
-%M = readtable("C:\Users\patri\Downloads\tok\01 tsharkPONDELOK4_6.csv");
+%M = readtable("C:\Users\patri\Downloads\časti_toku\piata_cast\piata_cast.csv");
 
 dlzka_csv = height(M)-1;
 
@@ -32,6 +30,16 @@ index = 1;
 data = zeros(1,dlzka_csv);
 data_kB = zeros(1,dlzka_csv);
 
+% medzery
+medzery = zeros(1,dlzka_csv);
+for i=1:dlzka_csv
+    if sekundy(i) > 1 && sekundy(i+1) < 1
+        medzery(i) = ( 60 - sekundy(i) ) + sekundy(i+1);
+        continue
+    end
+    medzery(i) = sekundy(i+1) - sekundy(i);
+end
+
 %slotovanie
 data_informacie = M.Var8;
 for i=1:dlzka_csv
@@ -39,7 +47,7 @@ for i=1:dlzka_csv
         if pom_m == minuty(i)
             if pom_s == sekundy_floored(i)
                 data(index) = data(index) + 1;
-                data_kB(index) = data_kB(index) + data_informacie(i); 
+                data_kB(index) = data_kB(index) + data_informacie(i);
                 continue
             end
         end
@@ -52,16 +60,6 @@ for i=1:dlzka_csv
     index = index + 1;
     data(index) = data(index) + 1;
     data_kB(index) = data_kB(index) + data_informacie(i);
-end
-
-% medzery
-medzery = zeros(1,dlzka_csv);
-for i=1:dlzka_csv
-    if sekundy(i) > 1 && sekundy(i+1) < 1
-        medzery(i) = ( 60 - sekundy(i) ) + sekundy(i+1);
-        continue
-    end
-    medzery(i) = sekundy(i+1) - sekundy(i);
 end
 
 % vypocty
@@ -151,7 +149,6 @@ title("\color{black}sikmost, \color{magenta}"+nasobok_spic+"x spicatost ,\color{
 subplot(subcislo2,1,3);
 histogram(data_plot_kB, 'Normalization', 'probability');
 title("histogram kB");
-
 
 %PRINTY
 m(1) = mean(data_plot); % klzavy priemer
