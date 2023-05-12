fe5cb28d878063c96e606eb6e56d72bd6f6c2582
superc1so <patrikcisar1999@gmail.com>
2023-05-12 23:08:18 +0200

    Update RED_poisson.m

--- Projekt/RED_poisson.m
+++ Projekt/RED_poisson.m
@@ -1,18 +1,22 @@
 clear
-clc
 
 pocet_generovanych = 10000;
 compute_window = 40;
 shift = 20;
 
 lambda_1 = 50;
-lambda_2 = 70;
-lambda_3 = 90;
+lambda_2 = 100;
+lambda_3 = 200;
 d = 0.1;
 Plost = 0.1;
 
+pravd_prepocitania = 0.5;
 min_th = 0.7;
 pravd_min_th = 0.7;
+typ_zahodenia = "linear";
+%typ_zahodenia = "logaritmus";
+%typ_zahodenia = "exponential";
+
 
 %generuj poisson
 data = zeros(1,pocet_generovanych);
@@ -38,8 +42,9 @@ klzavy_priemer = zeros(1,dlzka_dat);
 
 for i=compute_window+1:dlzka_dat-1
 
-    if mod(i,shift) ~= 0 % prejdu do vnutra vsetky okrem nasobkov shiftu..
-        [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th);
+    pom = n * pravd_prepocitania;
+    if q(i) < pom
+        [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th,typ_zahodenia);
         klzavy_priemer(i) = mean(data(i-compute_window:i));
         kapacita(i) = c;
         velkost_buffra(i) = n;
@@ -58,7 +63,7 @@ for i=compute_window+1:dlzka_dat-1
     end
 
     [c,n] = vypocitaj_poisson_kapacitu(lambda,Plost,d);
-    [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th);
+    [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th,typ_zahodenia);
 
     klzavy_priemer(i) = mean(data_cw);
     kapacita(i) = c;
@@ -71,9 +76,9 @@ end
 mean_zahodene = mean(zahodene);
 %mean_data = mean(data(1:count));
 
-zmena_toku = zeros(1,dlzka_dat);
-zmena_toku(dlzka_1) = lambda_3 + 50;
-zmena_toku(dlzka_1+dlzka_2) = lambda_3+50;
+% zmena_toku = zeros(1,dlzka_dat);
+% zmena_toku(dlzka_1) = lambda_3 + 50;
+% zmena_toku(dlzka_1+dlzka_2) = lambda_3+50;
 
 
 %Vypisy
@@ -84,8 +89,8 @@ hold on
 plot(klzavy_priemer);
 hold on
 plot(kapacita);
-hold on
-plot(zmena_toku);
+% hold on
+% plot(zmena_toku);
 title("data,"+"d="+d+", Plost="+Plost);
 legend('tok','klzavy priemer','kapacita','Location','northwest');
 xlim([0 dlzka_dat-1]);
@@ -110,17 +115,21 @@ xlim([0 dlzka_dat]);
 
 %fprintf("mean_data: %f\n",mean_data);
 zz = rand(1);
+fprintf("zahodene RED - "+typ_zahodenia+": %f\n",sum(zahodene_RED));
 
 
-function [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th)
+function [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahodene_RED,min_th,pravd_min_th,typ_zahodenia)
         
         %RED - skontrolovat kapacitu
         min_th_number =  min_th * n;
-        if q(i) > min_th_number
+        if q(i) > min_th_number % dnu pojde ked buffer > 80%
 
             for j=1:data(i+1)-c
+                pom_pravd_min_th = pravd_min_th;
+                [pom_pravd_min_th] = zisti_pravd_zahodenia_linear(q(i),pom_pravd_min_th,typ_zahodenia,n);
+
                 r = 1.*rand();
-                if r >= pravd_min_th
+                if r >= pom_pravd_min_th
                     %vlozi
                     q(i+1) = min(q(i+1) + 1, n);
                     je_viac = q(i+1) + 1;
@@ -129,7 +138,6 @@ function [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahode
                     end
                 else
                     %zahodi
-                    zahodene(i+1) = zahodene(i+1) + 1;
                     zahodene_RED(i+1) = zahodene_RED(i+1) + 1;
                 end
             end
@@ -143,7 +151,58 @@ function [q,zahodene,zahodene_RED] = vloz_do_buffra(data,q,zahodene,i,c,n,zahode
                 zahodene(i+1) = je_viac - n;
             end
         end
+end
 
+function [pravd_min_th] = zisti_pravd_zahodenia_linear(buffer,pravd_min_th,typ_zahodenia,n)
+
+    max_84 = 0.84 * n;
+    max_88 = 0.88 * n;
+    max_92 = 0.92 * n;
+    max_96 = 0.96 * n;
+
+    if buffer <= max_84
+        pravd_min_th = 0.70;% 70% pravd zahodenia
+    end
+
+    if buffer > max_84 && buffer <= max_88
+        if typ_zahodenia == "linear"
+            pravd_min_th = 0.75; 
+        end
+        if typ_zahodenia == "logaritmus"
+            pravd_min_th = 0.85;
+        end
+        if typ_zahodenia == "exponential"
+            pravd_min_th = 0.75; 
+        end
+    end
+
+    if buffer > max_88 && buffer <= max_92
+        if typ_zahodenia == "linear"
+            pravd_min_th = 0.80; 
+        end
+        if typ_zahodenia == "logaritmus"
+            pravd_min_th = 0.9;
+        end
+        if typ_zahodenia == "exponential"
+            pravd_min_th = 0.9; 
+        end
+    end
+
+    if buffer > max_92 && buffer <= max_96
+        if typ_zahodenia == "linear"
+            pravd_min_th = 0.85; 
+        end
+        if typ_zahodenia == "logaritmus"
+            pravd_min_th = 0.95;
+        end
+        if typ_zahodenia == "exponential"
+            pravd_min_th = 0.95; 
+        end
+    end
+
+    if buffer > max_96
+        pravd_min_th = 1; % 100% pravd zahodenia
+    end
 end
 
 function [data,dlzka_dat] = generuj_poisson(data, pocet_generovanych, lambda, dlzka_dat, was_here)
