74a334af6c383242105f5fe4932c0826b6a95b3d
superc1so <patrikcisar1999@gmail.com>
2023-12-07 22:38:09 +0100

    x

--- Projekt 3/test.m
+++ Projekt 3/test.m
@@ -1,2 +1,9 @@
-x = 5.5;
-disp(x)
+% Your value
+originalValue = 0.14;
+
+% Round up to two decimal places
+roundedValue = ceil(originalValue * 100) / 100;
+
+% Display the result
+disp(['Original Value: ', num2str(originalValue)]);
+disp(['Rounded Value: ', num2str(roundedValue)]);
